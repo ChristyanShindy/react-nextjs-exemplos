c2850c07e2a223e3b56ca608328f2c0ca8323094
ChristyanShindy <christyan@sattvaconsultoria.com.br>
2025-04-14 12:54:36 -0300

    imporatado fonte e configuração do site

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,22 @@
+import Head from "next/head"
 import Title from "../src/components/title/Title"
 import Subtitle from "../src/components/subtitle/Subtitle"
 
 
 function HomePage () {
     return (
+        <>
+        <Head>
+            <title>Teste fragment</title>
+        </Head>
         <div>
             <Title> um texto</Title>
             <Subtitle text="Um subtitulo"/>
             <h1>Hello World, NextJs + ReactJs</h1>
             <Subtitle />
-            <Title> outro texto</Title>
-            
+            <Title> outro texto</Title> 
         </div>
+        </>
     )
 }
 
