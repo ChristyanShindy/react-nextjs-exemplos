a0b66002deea98a791e53bdbe512a1209e9c167b
ChristyanShindy <christyan@sattvaconsultoria.com.br>
2025-04-21 22:44:00 -0300

    Criado Header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react"
 import Head from "next/head"
 import Title from "../src/components/title/Title"
 import Subtitle from "../src/components/subtitle/Subtitle"
+import Button from "../src/components/button/Button"
 
 
 function HomePage () {
+    const[click, setClick] = useState(0) 
+    const handleClick = () => {
+        setClick(click + 1)
+    }
+
     return (
         <>
-        <Head>
-            <title>Teste fragment</title>
-        </Head>
-        <div>
-            <Title> um texto</Title>
-            <Subtitle text="Um subtitulo"/>
-            <h1>Hello World, NextJs + ReactJs</h1>
-            <Subtitle />
-            <Title> outro texto</Title> 
-        </div>
+            <Head>
+                <title>Teste fragment</title>
+            </Head>
+            <div>
+                <Title> um texto</Title>
+                <Subtitle text="Um subtitulo" />
+                <h1>Hello World, NextJs + ReactJs</h1>
+                <Subtitle />
+                <Button
+                    onClick={handleClick}
+                >
+                    Botão Teste
+                </Button>
+                <p>Cliques: {click}</p>
+            </div>
         </>
     )
 }
