8788ce06867861f9dba74545a200b1550d16a1c5
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2025-05-14 19:59:29 -0300

    Criação Title

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,11 @@
+import Title from "../src/components/title/title"
+
 //Arquivo responsavel pela pagina inicial
 function HomePage () {
   return(
     <div>
       Hello  world, NextJs + ReactJS
+      <Title/>
     </div>
   )
 }
