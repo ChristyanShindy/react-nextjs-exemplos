74c64dfcb32870aada98cde43bfa2315d11c3895
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2025-05-16 13:31:59 -0300

    inserindo titulo e _document

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,16 @@
 //Arquivo que conseguimos fazer algumas configurações do next
 //Padrão Next para poder importar padrão Global
+import Head from 'next/head'
 import '../src/style.css'
 function App ({ Component, pageProps}) {
-  return <Component { ...pageProps}/> //Padrão Next para poder funcionar padrão Global
+  return (
+    <>
+      <Head>
+        <title>Titulo padrão</title>
+      </Head>
+      <Component { ...pageProps}/> //Padrão Next para poder funcionar padrão Global
+    </>
+  )
 }
 
 export default App
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,20 @@
+import Head from "next/head"
 import Title from "../src/components/title/title"
 import Subtitle from "../src/components/subtitle/Subtitle"
 //Arquivo responsavel pela pagina inicial
 function HomePage () {
   return(
-    <div>
-      <Title>Primeiro Titulo</Title>
-      <Subtitle text="Um Subtitulo"/>
-      <Title>Segundo Titulo </Title>
-      <Subtitle text="Outro Subtitulo"/>
-    </div>
+    <>
+      <Head>
+        <title>Titulo diferente da HomePage</title>
+      </Head>
+      <div>
+        <Title>Primeiro Titulo</Title>
+        <Subtitle text="Um Subtitulo"/>
+        <Title>Segundo Titulo </Title>
+        <Subtitle text="Outro Subtitulo"/>
+      </div>
+    </>
   )
 }
 
