6ff03f97fdb6eccaa95faf77442115639e9eba24
ChristyanShindy <christyan@sattvaconsultoria.com.br>
2025-05-17 22:10:16 -0300

    adicionado estado

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
 import Head from "next/head"
 import Title from "../src/components/title/title"
 import Subtitle from "../src/components/subtitle/Subtitle"
+import Button from "../src/components/button/Button"
 //Arquivo responsavel pela pagina inicial
 function HomePage () {
+  const [click, setClick] = useState(0)
+
+  const handleClick = () => {
+    console.log("Handle Click")
+    setClick(click + 1)
+  }
   return(
     <>
       <Head>
@@ -13,6 +21,12 @@ function HomePage () {
         <Subtitle text="Um Subtitulo"/>
         <Title>Segundo Titulo </Title>
         <Subtitle text="Outro Subtitulo"/>
+        <Button 
+          onClick={handleClick}
+        >
+          Botão teste
+        </Button>
+        <p>Cliques: {click} </p>
       </div>
     </>
   )
